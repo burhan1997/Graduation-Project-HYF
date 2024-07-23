ee6397d8e522615a6958f92c1add386db4198b26
hhana1-1 <hhana1@live.com>
2024-07-23 19:00:16 +0200

    updated name

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
-import Home from "./pages/Home/Home";
-import CreateUser from "./pages/User/CreateUser";
-import UserList from "./pages/User/UserList";
-import SignIn from "./pages/Sign-in/SignIn";
+import Home from "./pages/home/Home";
+import CreateUser from "./pages/user/CreateUser";
+import UserList from "./pages/user/UserList";
+import SignIn from "./pages/sign-in/SignIn";
 import CreateProfile from "./components/CreateProfile";
-import SignUp from "./pages/Sign_up/Sign_up";
+import SignUp from "./pages/sign_up/Sign_up";
 import "./index.css";
 
 const App = () => {
