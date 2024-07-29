b60f669241ddd86a3455db4c195cbaf16a144001
Hailemariam <htsigabu@gmail.com>
2024-07-29 11:05:19 +0200

    user list modified

--- client/src/components/user-card/UserCard.jsx
+++ client/src/components/user-card/UserCard.jsx
@@ -2,22 +2,49 @@ import React from "react";
 import "./UserCard.css";
 import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
+import calculateAge from "../../util/calculateAge";
+import { images } from "../../../public/assets/images";
 
 const UserCard = ({ user }) => {
   const navigate = useNavigate();
+  const names = [
+    "Alex",
+    "Casey",
+    "Jordan",
+    "Taylor",
+    "Morgan",
+    "Avery",
+    "Riley",
+    "Cameron",
+    "Jamie",
+    "Quinn",
+    "Dakota",
+    "Skyler",
+    "Parker",
+    "Reese",
+    "Payton",
+    "Hayden",
+    "Bailey",
+    "Rowan",
+    "Charlie",
+    "Emerson",
+  ];
 
   return (
     <div>
       <div className="usercard-content" onClick={(e) => e.stopPropagation()}>
-        <p className="usercard-h">{user.firstName + " " + user.lastName}</p>
-        <p className="usercard-p">{user.bio}</p>
-        <p className="usercard-p">{user.email}</p>
-        <p className="usercard-p">{user.hobbies}</p>
-        <img className="usercard-avatar" src={user.profile_picture} />
+        <p className="usercard-h">
+          {user.firstName
+            ? user.firstName
+            : names[Math.floor(Math.random() * 20)] +
+              ", " +
+              calculateAge(user.birthdate)}
+        </p>
+        <p className="usercard-p">{user.bio.slice(0, 30)}</p>
+        <img className="usercard-avatar" src={images.LogoGreen} />
         <button
           className="usercard-button-2"
-          //TODO add id to /user/profile${id}
-          onClick={() => navigate("/user/profile")}
+          onClick={() => navigate(`/user/${user._id}`)}
         >
           View full profile
         </button>
@@ -29,11 +56,13 @@ const UserCard = ({ user }) => {
 UserCard.propTypes = {
   user: PropTypes.shape({
     firstName: PropTypes.string.isRequired,
+    birthdate: PropTypes.string.isRequired,
     lastName: PropTypes.string.isRequired,
-    bio: PropTypes.number.isRequired,
+    bio: PropTypes.string.isRequired,
     email: PropTypes.string.isRequired,
     profile_picture: PropTypes.string.isRequired,
     hobbies: PropTypes.string.isRequired,
+    _id: PropTypes.string.isRequired,
   }).isRequired,
 };
 
