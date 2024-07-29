7c0b1b25619fceb24eb37155ec1b156d0b72c641
Hailemariam <htsigabu@gmail.com>
2024-07-29 11:23:35 +0200

    button styling and filling missing data

--- client/src/components/user-card/UserCard.jsx
+++ client/src/components/user-card/UserCard.jsx
@@ -29,18 +29,45 @@ const UserCard = ({ user }) => {
     "Charlie",
     "Emerson",
   ];
-
+  const activityStatuses = [
+    "Who wants to grab a pizza?",
+    "Anyone up for rollerblading?",
+    "Looking for a hiking buddy!",
+    "Let's go for a coffee!",
+    "Who's in for a movie night?",
+    "Game night, anyone?",
+    "Time for some beach volleyball!",
+    "Who wants to go cycling?",
+    "Up for a run in the park?",
+    "Picnic in the park?",
+    "Let's go bowling!",
+    "Anyone for a tennis match?",
+    "Who's free for a BBQ?",
+    "Ready for a road trip!",
+    "Who wants to go fishing?",
+    "Up for some rock climbing?",
+    "How about a dance party?",
+    "Let's hit the gym together!",
+    "Looking for a shopping buddy!",
+    "Who's in for a karaoke night?",
+  ];
+  const name = user.firstName
+    ? user.firstName
+    : names[Math.floor(Math.random() * 20)];
+  console.log("name", name);
+  const age = user.birthdate
+    ? calculateAge(user.birthdate)
+    : Math.floor(Math.random() * 80 + 18);
+  console.log(("age", age));
   return (
     <div>
       <div className="usercard-content" onClick={(e) => e.stopPropagation()}>
-        <p className="usercard-h">
-          {user.firstName
-            ? user.firstName
-            : names[Math.floor(Math.random() * 20)] +
-              ", " +
-              calculateAge(user.birthdate)}
+        <p className="usercard-h">{name + ", " + age}</p>
+        <p className="usercard-p">
+          {user.bio
+            ? user.bio.slice(0, 30)
+            : activityStatuses[Math.floor(Math.random() * 20)]}
         </p>
-        <p className="usercard-p">{user.bio.slice(0, 30)}</p>
         <img className="usercard-avatar" src={images.LogoGreen} />
         <button
           className="usercard-button-2"
