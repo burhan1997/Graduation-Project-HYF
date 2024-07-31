4195cc1c6b275c87e554dca02c0442a95e703eb4
Hailemariam <htsigabu@gmail.com>
2024-07-31 13:22:14 +0200

    button style changes

--- client/src/pages/user-list/UserList.jsx
+++ client/src/pages/user-list/UserList.jsx
@@ -26,13 +26,13 @@ function UserList() {
             {getUsersError && <p>{getUsersError.toString()}</p>}
           </div>
           <button className="arrow left-arrow" onClick={handlePrevPage}>
-            &#x2190;
+            &#x3c; {/*  &#x2190; */}
           </button>
           {users?.slice(page, page + 7).map((user) => (
             <UserCard key={user._id} user={user} />
           ))}
           <button className="arrow right-arrow" onClick={handleNextPage}>
-            &#x2192;
+            &#x3e;{/* &#x2192; */}
           </button>
         </div>
       </div>
