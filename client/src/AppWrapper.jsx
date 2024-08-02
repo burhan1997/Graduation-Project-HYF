9eb177f057def4452e35d5507d9696393dc2fe9d
MustafaTRKYLMZ <cana66331@gmail.com>
2024-08-02 17:36:39 +0200

    test setup completed

--- client/src/AppWrapper.jsx
+++ client/src/AppWrapper.jsx
@@ -4,16 +4,31 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { FormProvider } from "./context/formContext";
 import { UsersProvider } from "./context/usersContext";
 import { UserProvider } from "./context/userContext";
+import { SendBirdProvider } from "@sendbird/uikit-react";
+import "@sendbird/uikit-react/dist/index.css";
 
 /**
  * This component wraps our App with the providers we do not want to have in our tests
  */
+//  <SelectBirdProvider>
+
 const AppWrapper = ({ children }) => {
   return (
     <Router>
       <UsersProvider>
         <FormProvider>
-          <UserProvider>{children}</UserProvider>
+          <UserProvider>
+            <SendBirdProvider
+              appId={process.env.REACT_APP_SENDBIRD_APP_ID}
+              userId={"new_user_id_123"}
+              nickname={"123"}
+              theme="dark"
+              //             onUserLeaveChannel={() => console.log('User left the channel')}
+              // onSendUserMessage={(message) => console.log('Sent message:', message)}
+            >
+              {children}
+            </SendBirdProvider>
+          </UserProvider>
         </FormProvider>
       </UsersProvider>
     </Router>
---
+++ client/src/pages/chat/Chat.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+import SBConversation from "@sendbird/uikit-react/GroupChannel";
+import SBChannelList from "@sendbird/uikit-react/GroupChannelList";
+import SBChannelSettings from "@sendbird/uikit-react/ChannelSettings";
+//import SBCreateChannel from "@sendbird/uikit-react/CreateChannel"
+
+import "./Chat.css";
+
+export const Chat = () => {
+  // useState
+  //const [targetUserId, setTargetUserID] = useState("targetUserID")
+  const [currentChannelUrl, setCurrentChannelUrl] = useState(null);
+  const [showSettings, setShowSettings] = useState(false);
+
+  return (
+    <div className="customized-app">
+      <div className="sendbird-app__wrap">
+        <div className="sendbird-app__channellist-wrap">
+          <SBChannelList
+            selectedChannelUrl={currentChannelUrl}
+            onChannelCreated={(channel) => {
+              setCurrentChannelUrl(channel.url);
+            }}
+            onChannelSelect={(channel) => {
+              setCurrentChannelUrl(channel?.url);
+            }}
+          />
+        </div>
+        <div className="sendbird-app__conversation-wrap">
+          <SBConversation
+            channelUrl={currentChannelUrl}
+            onChatHeaderActionClick={() => {
+              setShowSettings(true);
+            }}
+          />
+        </div>
+      </div>
+      {showSettings && (
+        <div className="sendbird-app__settingspanel-wrap">
+          <SBChannelSettings
+            channelUrl={currentChannelUrl}
+            onCloseClick={() => {
+              setShowSettings(false);
+            }}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
